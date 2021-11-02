b23d069ab342f1e6884dcceb4881fa4d653dee32
cwhite911 <smortopahri@gmail.com>
2021-11-02 15:33:31 -0400

    Updated gitignore

--- api/settings.py
+++ api/settings.py
@@ -86,7 +86,7 @@ if DEBUG:
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:8000',
-    'http://webapp:3000'
+    'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'api.urls'
--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      activeItem: {
+        title: "",
+        description: "",
+        completed: false
+      },
+      todoList: []
+      };
+  }
+
+    async componentDidMount() {
+      try {
+        const res = await fetch('http://localhost:8000/world/countries/');
+        const todoList = await res.json();
+        console.log("TODO:", todoList.results.features)
+        const todoListFeatures = todoList.results.features
+        this.setState({
+          todoList: todoListFeatures
+        });
+        
+      } catch (e) {
+        console.log(e);
+    }
+    }
+    renderItems = () => {
+      const { viewCompleted } = this.state;
+      const newItems = this.state.todoList;
+      console.log("Render Items", newItems)
+      return newItems.map(item => (
+        <li 
+          key={item.id}
+          className="list-group-item d-flex justify-content-between align-items-center"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          <span 
+            className={`todo-title mr-2 ${
+              this.state.viewCompleted ? "completed-todo" : ""
+            }`}
+            title={item.properties.area}
+            >
+              {item.properties.name} <br></br>
+              Population: {item.properties.pop2005} <br></br>
+              Area: {item.properties.area} <br></br>
+            </span>
+        </li>
+      ));
+    };
 
+    render() {
+      return (
+        <main className="content">
+        <div className="row">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <ul className="list-group list-group-flush">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+      )
+    }
+  }
+  
 export default App;
