fedaed95a1c7474b4c0817f656141407a9953942
Corey White <smortopahri@gmail.com>
2022-03-11 01:55:23 -0500

    Added jupyter notebook to play with actinia

---
+++ notebooks/scripts/install_grass_plugins.sh
@@ -0,0 +1,11 @@
+g.extension extension=r.futures.pga
+g.extension extension=r.futures.potential
+g.extension extension=r.futures.potsurface
+g.extension extension=r.futures.demand
+g.extension extension=r.futures.devpressure
+g.extension extension=r.futures.category
+
+
+
+
+
