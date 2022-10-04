ca992964e1247aa6a44c42d57c3c2cc606954541
Corey White <smortopahri@gmail.com>
2022-10-04 13:38:18 -0400

    Added OpenPlains banner to login view

--- webapp/src/containers/Auth/AuthContainer.jsx
+++ webapp/src/containers/Auth/AuthContainer.jsx
@@ -11,16 +11,20 @@ import './auth.scss'
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
-
+import Image from 'react-bootstrap/Image'
 
 export const AuthContainer = ({children}) => {
 
 
     return(
-        <Container className={"min-vh-100"} style={{marginTop:'-50px'}}>
+        <Container className={"text-dark"} style={{marginTop:'-50px', height: '100vh'}}>
             <Row className="min-vh-100 justify-content-center align-items-center">
+                
                 <Col/>
-                <Col>{children}</Col>
+                <Col>
+                <Image fluid src="./banner_v1.png"></Image>
+                {children}
+                </Col>
                 <Col/>
             </Row>
         </Container>
